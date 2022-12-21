337bd5dd014d8d4c113563756d180cb62ce0500d
muyi-jian <2628168756@qq.com>
2022-12-21 20:24:05 +0800

    JdbcTemplate代码示例

---
+++ spring-boot-JdbcTemplate/src/main/java/com/yj/springboot/SpringBootJdbcTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.yj.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdbcTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJdbcTemplateApplication.class, args);
+    }
+
+}
---
+++ spring-boot-JdbcTemplate/src/test/java/com/yj/springboot/SpringBootJdbcTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJdbcTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
