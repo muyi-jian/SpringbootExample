a0f240d6e262dc4ad15a0400e860b096a5da7bb9
muyi_jian <2628168756@qq.com>
2023-10-09 16:51:30 +0800

    springboot3集成swagger

---
+++ spring-boot3-swagger/src/main/java/com/yj/swagger/SpringBoot3SwaggerApplication.java
@@ -0,0 +1,16 @@
+package com.yj.swagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+        访问地址：
+        http://localhost:9000/test-swagger/swagger-ui/index.html
+ */
+@SpringBootApplication
+public class SpringBoot3SwaggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3SwaggerApplication.class, args);
+    }
+
+}
---
+++ spring-boot3-swagger/src/test/java/com/yj/swagger/SpringBoot3SwaggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.swagger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3SwaggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
