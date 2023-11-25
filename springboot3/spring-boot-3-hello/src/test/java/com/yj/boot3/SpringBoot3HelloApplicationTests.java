47f8276fa012091bf8212414809ff0e0b62d9d55
muyi-jian <2628168756@qq.com>
2023-11-25 18:29:59 +0800

    提交3.x版本代码

---
+++ springboot3/spring-boot-3-hello/src/test/java/com/yj/boot3/SpringBoot3HelloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.boot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3HelloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
