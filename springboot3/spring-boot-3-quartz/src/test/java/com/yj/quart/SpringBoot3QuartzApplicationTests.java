b2e922d49e7f5f1e5f518c996086bac0e1bdd838
muyi-jian <2628168756@qq.com>
2024-01-02 16:16:24 +0800

    整合QUARTZ作业调度任务

---
+++ springboot3/spring-boot-3-quartz/src/test/java/com/yj/quart/SpringBoot3QuartzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.quart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3QuartzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
